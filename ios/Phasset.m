29f52d6b873c069a629bbcc283291ca7ac5dbbf0
Tuan Luong <tuanluong.it@gmail.com>
2019-10-07 10:47:46 +0700

    update request image

--- ios/Phasset.m
+++ ios/Phasset.m
@@ -22,7 +22,7 @@ RCT_EXPORT_METHOD(requestImage:(NSDictionary *)params
     
     NSArray* localIds = [NSArray arrayWithObjects: assetId, nil];
     PHAsset * _Nullable asset = [PHAsset fetchAssetsWithLocalIdentifiers:localIds options:nil].firstObject;
-    CGSize targetSize = CGSizeMake(1024, 1024);
+    CGSize targetSize = CGSizeMake(512, 512);
     
     [manager requestImageForAsset:(PHAsset *)asset
                        targetSize:(CGSize)targetSize
@@ -36,76 +36,57 @@ RCT_EXPORT_METHOD(requestImage:(NSDictionary *)params
         }
         
         NSData *const imageData = UIImageJPEGRepresentation(image, 1.0);
-        NSInteger const length = [imageData length];
         
         NSURL *sourceURL = [info objectForKey:@"PHImageFileURLKey"];
         
-        NSDictionary* exif = nil;
-        if([[params objectForKey:@"includeExif"] boolValue]) {
-            exif = [[CIImage imageWithData:imageData] properties];
-        }
-
-        NSString *filePath = @"";
-        NSDictionary* result = [self createAttachmentResponse:filePath
-                                                     withExif: exif
-                                                withSourceURL:[sourceURL absoluteString]
-                                          withLocalIdentifier: asset.localIdentifier
-                                                 withFilename: [asset valueForKey:@"filename"]
-                                                    withWidth:@(1024)
-                                                   withHeight:@(1024)
-                                                     withMime:@"image/jpeg"
-                                                     withSize:[NSNumber numberWithUnsignedInteger:imageData.length]
-                                                     withData:[[params objectForKey:@"includeBase64"] boolValue] ? [imageData base64EncodedStringWithOptions:0]: nil
-                                                     withRect:CGRectNull
-                                             withCreationDate:asset.creationDate
-                                         withModificationDate:asset.modificationDate
-                                   ];
-        resolve(result);
+        NSDictionary* exif = [[CIImage imageWithData:imageData] properties];
+        
+        NSString *filePath = [self saveFile:imageData];
+        NSDictionary* data = @{
+            @"id": asset.localIdentifier,
+            @"path": (filePath && ![filePath isEqualToString:(@"")]) ? filePath : [NSNull null],
+            @"sourceURL": (sourceURL) ? sourceURL : [NSNull null],
+            @"localIdentifier": asset.localIdentifier,
+            @"filename": [asset valueForKey:@"filename"],
+            @"width": [exif valueForKey:@"PixelWidth"],
+            @"height": [exif valueForKey:@"PixelHeight"],
+            @"mime": @"image/jpeg",
+            @"size": [NSNumber numberWithUnsignedInteger:imageData.length],
+            @"exif": (exif) ? exif : [NSNull null],
+            @"creationDate": (asset.creationDate) ? [NSString stringWithFormat:@"%.0f", [asset.creationDate timeIntervalSince1970]] : [NSNull null],
+            @"modificationDate": (asset.modificationDate) ? [NSString stringWithFormat:@"%.0f", [asset.modificationDate timeIntervalSince1970]] : [NSNull null],
+        };
+        
+        resolve(data);
     }];
-//    [manager requestImageDataForAsset:(PHAsset *)asset options:options resultHandler:^(NSData * _Nullable imageData, NSString * _Nullable dataUTI, UIImageOrientation orientation, NSDictionary * _Nullable info) {
-//        NSURL *sourceURL = [info objectForKey:@"PHImageFileURLKey"];
-//
-//        dispatch_async(dispatch_get_main_queue(), ^{
-//            @autoreleasepool {
-//                UIImage *imgT = [UIImage imageWithData:imageData];
-//
-//                Boolean forceJpg = [[params valueForKey:@"forceJpg"] boolValue];
-//
-//                NSNumber *compressQuality = [params valueForKey:@"compressImageQuality"];
-//                Boolean isLossless = (compressQuality == nil || [compressQuality floatValue] >= 0.8);
-//
-//                NSNumber *maxWidth = [params valueForKey:@"compressImageMaxWidth"];
-//                Boolean useOriginalWidth = (maxWidth == nil || [maxWidth integerValue] >= imgT.size.width);
-//
-//                NSNumber *maxHeight = [params valueForKey:@"compressImageMaxHeight"];
-//                Boolean useOriginalHeight = (maxHeight == nil || [maxHeight integerValue] >= imgT.size.height);
-//
-//                NSString *mimeType = [self determineMimeTypeFromImageData:imageData];
-//                Boolean isKnownMimeType = [mimeType length] > 0;
-//
-//                NSDictionary* exif = nil;
-//                if([[params objectForKey:@"includeExif"] boolValue]) {
-//                    exif = [[CIImage imageWithData:imageData] properties];
-//                }
-//
-//                NSString *filePath = @"";
-//                NSDictionary* result = [self createAttachmentResponse:filePath
-//                                                            withExif: exif
-//                                                       withSourceURL:[sourceURL absoluteString]
-//                                                 withLocalIdentifier: asset.localIdentifier
-//                                                        withFilename: [asset valueForKey:@"filename"]
-//                                                           withWidth:@(imgT.size.width)
-//                                                          withHeight:@(imgT.size.height)
-//                                                            withMime:mimeType
-//                                                            withSize:[NSNumber numberWithUnsignedInteger:imageData.length]
-//                                                            withData:[[params objectForKey:@"includeBase64"] boolValue] ? [imageData base64EncodedStringWithOptions:0]: nil
-//                                                            withRect:CGRectNull
-//                                                    withCreationDate:asset.creationDate
-//                                                withModificationDate:asset.modificationDate
-//                                       ];
-//            }
-//        });
-//    }];
+}
+
+- (NSString*) getTmpDirectory {
+    NSString *TMP_DIRECTORY = @"react-native-phasset/";
+    NSString *tmpFullPath = [NSTemporaryDirectory() stringByAppendingString:TMP_DIRECTORY];
+
+    BOOL isDir;
+    BOOL exists = [[NSFileManager defaultManager] fileExistsAtPath:tmpFullPath isDirectory:&isDir];
+    if (!exists) {
+        [[NSFileManager defaultManager] createDirectoryAtPath: tmpFullPath
+                                  withIntermediateDirectories:YES attributes:nil error:nil];
+    }
+
+    return tmpFullPath;
+}
+
+- (NSString*) saveFile:(NSData*)data {
+    NSString *tmpDir = [self getTmpDirectory];
+    NSString *filePath = [tmpDir stringByAppendingString:[[NSUUID UUID] UUIDString]];
+    filePath = [filePath stringByAppendingString:@".jpg"];
+
+    // save cropped file
+    BOOL status = [data writeToFile:filePath atomically:YES];
+    if (!status) {
+        return nil;
+    }
+
+    return filePath;
 }
 
 // See https://stackoverflow.com/questions/4147311/finding-image-type-from-nsdata-or-uiimage
@@ -125,23 +106,6 @@ RCT_EXPORT_METHOD(requestImage:(NSDictionary *)params
             return @"image/tiff";
     }
     return @"";
-}
-
-- (NSDictionary*) createAttachmentResponse:(NSString*)filePath withExif:(NSDictionary*) exif withSourceURL:(NSString*)sourceURL withLocalIdentifier:(NSString*)localIdentifier withFilename:(NSString*)filename withWidth:(NSNumber*)width withHeight:(NSNumber*)height withMime:(NSString*)mime withSize:(NSNumber*)size withData:(NSString*)data withRect:(CGRect)cropRect withCreationDate:(NSDate*)creationDate withModificationDate:(NSDate*)modificationDate {
-    return @{
-             @"path": (filePath && ![filePath isEqualToString:(@"")]) ? filePath : [NSNull null],
-             @"sourceURL": (sourceURL) ? sourceURL : [NSNull null],
-             @"localIdentifier": (localIdentifier) ? localIdentifier : [NSNull null],
-             @"filename": (filename) ? filename : [NSNull null],
-             @"width": width,
-             @"height": height,
-             @"mime": mime,
-             @"size": size,
-             @"data": (data) ? data : [NSNull null],
-             @"exif": (exif) ? exif : [NSNull null],
-             @"creationDate": (creationDate) ? [NSString stringWithFormat:@"%.0f", [creationDate timeIntervalSince1970]] : [NSNull null],
-             @"modificationDate": (modificationDate) ? [NSString stringWithFormat:@"%.0f", [modificationDate timeIntervalSince1970]] : [NSNull null],
-             };
 }
 
 @end
