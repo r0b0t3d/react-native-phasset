dee1f16fe6a598d5aa2411349fa566bdcceb0726
Tuan Luong <tuanluong.it@gmail.com>
2020-01-06 16:09:36 +0700

    add android

---
+++ android/src/main/java/com/tl/ImageResizer.java
@@ -0,0 +1,126 @@
+package com.tl;
+
+import android.content.Context;
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.MediaStore;
+import android.util.Base64;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.IOException;
+import java.util.Date;
+
+/**
+ * Provide methods to resize and rotate an image file.
+ */
+public class ImageResizer {
+    public static void saveImage(String filePath, Bitmap bitmap) {
+        FileOutputStream out = null;
+        try {
+            out = new FileOutputStream(filePath);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 70, out);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (out != null) {
+                    out.close();
+                }
+            } catch (Exception e1) {
+            }
+        }
+    }
+
+    public static Bitmap decodeSampledBitmap(Context context, Uri photoUri, int reqWidth, int reqHeight) {
+        try {
+            // Decode with inJustDecodeBounds = true to check dimensions
+            final BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inJustDecodeBounds = true;
+            InputStream imageStream = context.getContentResolver().openInputStream(photoUri);
+            BitmapFactory.decodeStream(imageStream, null, options);
+            imageStream.close();
+            // Calculate inSampleSize
+            options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+            // Decode bitmap with inSampleSize
+            options.inJustDecodeBounds = false;
+            imageStream = context.getContentResolver().openInputStream(photoUri);
+            Bitmap bitmap = BitmapFactory.decodeStream(imageStream, null, options);
+            return rotateImageIfRequired(context, bitmap, photoUri);
+        } catch (IOException exception) {
+            exception.printStackTrace();
+            return null;
+        }
+    }
+
+    public static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keep boths
+            // height and width larger then the requested height and width
+            while ((halfHeight / inSampleSize) > reqHeight
+                    && (halfWidth / inSampleSize) > reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+        return inSampleSize;
+    }
+
+    public static Bitmap scaleDown(Bitmap realImage, float maxImageSize,
+                                   boolean filter) {
+        float ratio = Math.min(
+                maxImageSize / realImage.getWidth(),
+                maxImageSize / realImage.getHeight());
+        int width = Math.round(ratio * realImage.getWidth());
+        int height = Math.round(ratio * realImage.getHeight());
+
+        Bitmap newBitmap = Bitmap.createScaledBitmap(realImage, width,
+                height, filter);
+        return newBitmap;
+    }
+
+    private static Bitmap rotateImageIfRequired(Context context, Bitmap img, Uri selectedImage) throws IOException {
+        InputStream input = context.getContentResolver().openInputStream(selectedImage);
+        ExifInterface ei;
+        if (Build.VERSION.SDK_INT > 23)
+            ei = new ExifInterface(input);
+        else
+            ei = new ExifInterface(selectedImage.getPath());
+
+        int orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+
+        switch (orientation) {
+            case ExifInterface.ORIENTATION_ROTATE_90:
+                return rotateImage(img, 90);
+            case ExifInterface.ORIENTATION_ROTATE_180:
+                return rotateImage(img, 180);
+            case ExifInterface.ORIENTATION_ROTATE_270:
+                return rotateImage(img, 270);
+            default:
+                return img;
+        }
+    }
+
+    private static Bitmap rotateImage(Bitmap img, int degree) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(degree);
+        Bitmap rotatedImg = Bitmap.createBitmap(img, 0, 0, img.getWidth(), img.getHeight(), matrix, true);
+        img.recycle();
+        return rotatedImg;
+    }
+}
---
+++ android/src/main/java/com/tl/PhassetModule.java
@@ -0,0 +1,95 @@
+package com.tl;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.AsyncTask;
+
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.GuardedAsyncTask;
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.WritableMap;
+
+import java.io.File;
+import java.io.IOException;
+
+public class PhassetModule extends ReactContextBaseJavaModule {
+
+    private final ReactApplicationContext reactContext;
+
+    public PhassetModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
+    }
+
+    @Override
+    public String getName() {
+        return "Phasset";
+    }
+
+    @ReactMethod
+    public void requestImage(ReadableMap params, final Promise promise) {
+        // Run in guarded async task to prevent blocking the React bridge
+        new ResizeImageTask(reactContext, params, promise).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+    }
+
+    static class ResizeImageTask extends GuardedAsyncTask<Void, Void> {
+
+        private final Context context;
+        private final ReadableMap params;
+        private final Promise promise;
+
+        protected ResizeImageTask(ReactContext reactContext, ReadableMap params, Promise promise) {
+            super(reactContext.getExceptionHandler());
+            this.context = reactContext;
+            this.params = params;
+            this.promise = promise;
+        }
+
+        @Override
+        protected void doInBackgroundGuarded(Void... voids) {
+            final String uri = params.getString("uri");
+            final int maxWidth = params.hasKey("maxWidth") ? params.getInt("maxWidth") : 1024;
+            final int maxHeight = params.hasKey("maxHeight") ? params.getInt("maxHeight") : 1024;
+            try {
+                WritableMap result = createResizedImageWithExceptions(uri, maxWidth, maxHeight);
+                promise.resolve(result);
+            }
+            catch (IOException e) {
+                promise.reject(e);
+            }
+        }
+
+        private WritableMap createResizedImageWithExceptions(String imagePath, int maxWidth, int maxHeight) throws IOException {
+            Bitmap.CompressFormat compressFormat = Bitmap.CompressFormat.JPEG;
+            Uri imageUri = Uri.parse(imagePath);
+
+            Bitmap scaleBitmap = ImageResizer.decodeSampledBitmap(context, imageUri, maxWidth, maxHeight);
+
+            if (scaleBitmap == null) {
+                throw new IOException("The image failed to be resized; invalid Bitmap result.");
+            }
+
+            // Save the resulting image
+            String fileName = System.currentTimeMillis() + "." + compressFormat.name();
+            File result = new File(context.getCacheDir(), fileName);
+            ImageResizer.saveImage(result.getAbsolutePath(), scaleBitmap);
+
+            WritableMap response = Arguments.createMap();
+            response.putString("path", result.getAbsolutePath());
+            response.putString("uri", Uri.fromFile(result).toString());
+            response.putString("name", result.getName());
+            response.putDouble("size", result.length());
+            response.putDouble("width", scaleBitmap.getWidth());
+            response.putDouble("height", scaleBitmap.getHeight());
+            // Clean up bitmap
+            scaleBitmap.recycle();
+            return response;
+        }
+    }
+}
